b9b5e51f3edc9792246340ddcea337516346f8bf
reizaakbar <reiwildani@gmail.com>
2024-10-01 18:01:37 +0700

    feat:set up landing-page

---
+++ src/components/sub/UlasanContent.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+import UlasanCarousel from "../carouselUlasan/CarouselUlasan";
+
+const UlasanContent = () => {
+  return <UlasanCarousel />;
+};
+
+export default UlasanContent;
