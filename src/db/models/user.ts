c1b05bf89d5a31299ca79c44932b87f43fde3df4
unknown <dickytaruna67@gmail.com>
2024-10-01 21:25:56 +0700

    profile-action

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -8,6 +8,8 @@ export type UserModel = {
   username: string;
   email: string;
   password: string;
+  following: object[];
+  followers: object[];
 };
 
 export type RegisterInput = Omit<UserModel, "_id">;
@@ -32,47 +34,63 @@ export const doRegister = async (user: RegisterInput) => {
   const result = await db.collection(COLLECTION_NAME).insertOne(modifiedUser);
 
   return result;
-  
 };
 
 export const getUserByEmail = async (email: string) => {
-  const db = await getDb()
-  const user = (await db.collection(COLLECTION_NAME).findOne({email: email})) as UserModel
+  const db = await getDb();
+  const user = (await db
+    .collection(COLLECTION_NAME)
+    .findOne({ email: email })) as UserModel;
 
-  return user
-}
-
-export const getUserByUsername = async (username: string) => {
-  const db = await getDb()
-  const user = (await db.collection(COLLECTION_NAME).findOne({username: username})) as UserModel
-
-  return user
-}
-
-export const doLogin = async (usernameOrEmail: string, password: string) => {
- 
-  const user =
-    usernameOrEmail.includes("@")
-      ? await getUserByEmail(usernameOrEmail)
-      : await getUserByUsername(usernameOrEmail);
-
-  if (!user) {
-    return null; 
-  }
-
-  
-  const isPasswordValid = comparePasswordWithHash(password, user.password);
-  return isPasswordValid ? user : null; 
+  return user;
 };
 
+export const getUserByUsername = async (username: string) => {
+  const db = await getDb();
+  const user = (await db
+    .collection(COLLECTION_NAME)
+    .findOne({ username: username })) as UserModel;
 
-export const getProfileById = async(_id: string) => {
-  const db = await getDb()
-  const objectId = new ObjectId(_id)
+  return user;
+};
 
-  const user = (await db.collection(COLLECTION_NAME).findOne({_id:objectId}, {projection: { password: 0 }})) as UserModel
+export const doLogin = async (usernameOrEmail: string, password: string) => {
+  const user = usernameOrEmail.includes("@")
+    ? await getUserByEmail(usernameOrEmail)
+    : await getUserByUsername(usernameOrEmail);
 
-  return user
-}
+  if (!user) {
+    return null;
+  }
 
+  const isPasswordValid = comparePasswordWithHash(password, user.password);
+  return isPasswordValid ? user : null;
+};
 
+export const getProfileById = async (_id: string) => {
+  const db = await getDb();
+  const objectId = new ObjectId(_id);
+
+  const user = (await db
+    .collection(COLLECTION_NAME)
+    .findOne({ _id: objectId }, { projection: { password: 0 } })) as UserModel;
+
+  return user;
+};
+
+export const followUSer = async (userId: string, followersId: string) => {
+  const db = await getDb();
+  await db
+    .collection(COLLECTION_NAME)
+    .updateOne(
+      { _id: new ObjectId() },
+      { $addToSet: { followers: new ObjectId(userId) } }
+    );
+
+  await db
+    .collection(COLLECTION_NAME)
+    .updateOne(
+      { _id: new ObjectId() },
+      { $addToSet: { following: new ObjectId(followersId) } }
+    );
+};
