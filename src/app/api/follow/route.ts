dab848b02b9c00a2c3acb2cedd9051a41b914934
reizaakbar <reiwildani@gmail.com>
2024-10-03 16:11:36 +0700

    added monaco

---
+++ src/app/api/follow/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { followUSer } from "@/db/models/user";
+import { readPayload } from "@/lib/jwt";
+
+export async function POST(request: Request) {
+  try {
+    const authHeader = request.headers.get("Authorization");
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return NextResponse.json(
+        { error: "Token tidak ditemukan" },
+        { status: 401 }
+      );
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const payload = readPayload(token);
+    const userId = payload.id;
+
+    const { followUserId } = await request.json();
+
+    if (!followUserId) {
+      return NextResponse.json({ error: "userId dan followersId os required" });
+    }
+
+    await followUSer(userId, followUserId);
+
+    return NextResponse.json(
+      { message: "Successfully followed user" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "error occurred while processing the request" },
+      { status: 500 }
+    );
+  }
+}
