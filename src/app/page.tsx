1236504059b093bae034365b9e4df9dd420999e4
Gerald Simanullang <geraldsimanullang@gmail.com>
2024-10-04 13:19:18 +0700

    removed start background and ulasan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
 import Footer from "@/components/main/Footer";
 import Hero from "@/components/main/Hero";
 import Navbar from "@/components/main/Navbar";
-import StarsCanvas from "@/components/main/StarBackground";
-import UlasanContent from "@/components/sub/UlasanContent";
-import Dashboard from "./dashboard/page";
-import Profile from "./profile/profile";
-import CodeEditorPage from "./sandbox/page";
 
 export default function Home() {
   return (
     <div className="h-full w-full">
-      {/* <StarsCanvas /> */}
       <div className="flex flex-col gap-56">
-        {/* <Navbar />
+        <Navbar />
         <Hero />
-        <UlasanContent />
-        <Footer /> */}
-        {/* <Dashboard /> */}
-        {/* <Profile /> */}
-        {/* <Dashboard /> */}
-        <CodeEditorPage />
+        <Footer />
       </div>
     </div>
   );
