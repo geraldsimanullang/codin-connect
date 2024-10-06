76dfdb426e320b486ea17224b9e2a72b6d349f44
unknown <dickytaruna67@gmail.com>
2024-10-06 19:05:02 +0700

    fixed-register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { slideInFromLeft, slideInFromRight } from "@/utils/motion";
 import { LuLaugh } from "react-icons/lu";
+import { handleRegister } from "./action";
 
 const Register = () => {
   return (
@@ -57,7 +58,11 @@ const Register = () => {
               <div className="absolute -top-3 -right-3">
                 <LuLaugh className="text-5xl fill-blue-700 stroke-gray-200 rotate-45" />
               </div>
-              <div className="flex flex-col items-center z-10">
+
+              <form 
+              action={handleRegister}
+              method="POST"
+              className="flex flex-col items-center z-10">
                 <h1 className="text-4xl font-bold mb-2">Register</h1>
                 <p className="text-gray-600 mb-6">Sign up to get started</p>
 
@@ -66,6 +71,7 @@ const Register = () => {
                     <i className="fas fa-user absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
                     <input
                       type="text"
+                      name="name"
                       placeholder="Name"
                       className="pl-10 pr-4 py-2 w-full rounded-full shadow-md focus:outline-none"
                     />
@@ -75,6 +81,7 @@ const Register = () => {
                     <i className="fas fa-user-circle absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
                     <input
                       type="text"
+                      name="username"
                       placeholder="Username"
                       className="pl-10 pr-4 py-2 w-full rounded-full shadow-md focus:outline-none"
                     />
@@ -84,6 +91,7 @@ const Register = () => {
                     <i className="fas fa-envelope absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
                     <input
                       type="email"
+                      name="email"
                       placeholder="Email"
                       className="pl-10 pr-4 py-2 w-full rounded-full shadow-md focus:outline-none"
                     />
@@ -93,6 +101,7 @@ const Register = () => {
                     <i className="fas fa-lock absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
                     <input
                       type="password"
+                      name="password"
                       placeholder="Password"
                       className="pl-10 pr-4 py-2 w-full rounded-full shadow-md focus:outline-none"
                     />
@@ -102,7 +111,7 @@ const Register = () => {
                 <button className="bg-gradient-to-r from-blue-600 to-purple-700 text-white py-2 px-6 rounded-full shadow-md">
                   Sign up
                 </button>
-              </div>
+              </form>
 
               <p className="text-xs pr-1 text-center mt-4">
                 *By creating an account, you agree to our terms and conditions.
