d6e92c7e07fb8f996bc9623e944597a778a78048
unknown <dickytaruna67@gmail.com>
2024-10-09 01:09:33 +0700

    search

---
+++ src/app/api/profile/search/user/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { searchUserByUsername } from "@/db/models/user";
+// Menangani permintaan GET
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const username = searchParams.get("username");
+
+  console.log(username);
+
+  if (!username) {
+    return NextResponse.json(
+      { error: "Username is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const user = await searchUserByUsername(username);
+
+    console.log(user);
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "An error occurred while searching for the user." },
+      { status: 500 }
+    );
+  }
+}
